4b870940d08b2c196a067640fb2e3ef722502f4d
bollen <david.boll@me.com>
2018-02-14 14:46:01 +0100

    updated enviorment-file

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,9 @@
-# Set up gems listed in the Gemfile.
-# See: http://gembundler.com/bundler_setup.html
-#      http://stackoverflow.com/questions/7243486/why-do-you-need-require-bundler-setup
+#config/environment.rb
+
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
-# Require gems we care about
 require 'rubygems'
 require 'uri'
 require 'pathname'
@@ -20,26 +18,25 @@ require 'pry' unless production?
 
 require 'erb'
 
-# Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 
-APP_NAME = APP_ROOT.basename.to_s
+APP_NAME = 'slow_food'
 
-configure do
-  # By default, Sinatra assumes that the root is the file that calls the configure block.
-  # Since this is not the case for us, we set it manually.
-  set :root, APP_ROOT.to_path
-  # See: http://www.sinatrarb.com/faq.html#sessions
+require APP_ROOT.join('config', 'database')
+
+
+class SlowFoodApp < Sinatra::Base
+  disable :logger, :dump_errors
   enable :sessions
-  set :session_secret, ENV['SESSION_SECRET'] || 'this is a secret shhhhh'
-
-  # Set the views to
-  set :views, File.join(Sinatra::Application.root, "app", "views")
+  register Sinatra::Flash
+  helpers Sinatra::RedirectWithFlash
+  set :session_secret, ENV['SESSION_SECRET'] || '1234ewqweert452233'
+  set :method_override, true
+  set :root, APP_ROOT
+  set :views, File.join(APP_ROOT, 'app', 'views')
+  set :public_folder, File.join(APP_ROOT, 'public')
+  set :show_exceptions, false
 end
 
-# Set up the controllers and helpers
 Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| require file }
 Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
-
-# Set up the database and models
-require APP_ROOT.join('config', 'database')
