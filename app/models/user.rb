4c80fdfffa4d5f2d11ed65301f4ea0c7bfade8e9
Antonella Morittu <morittuantonella@gmail.com>
2018-02-14 17:28:14 +0100

    column name in menu.rb tested

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,3 @@ class User < ActiveRecord::Base
   has_secure_password
   # Remember to create a migration!
 end
-
-class CreateUsers < ActiveRecord::Migration
-  def change
-    create_table :users do |t|
-      t.string :name
-      t.string :password_digest
-    end
-  end
-end
