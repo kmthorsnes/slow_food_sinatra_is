f6cb50374bcda41492a5bec9c8879ab78d20272e
kalle <kmthorsnes@gmail.com>
2018-02-16 18:48:31 +0100

    created categories step definitions

--- spec/categories_spec.rb
+++ spec/categories_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe Menu do
-  it { is_expected.to have_db_column :name }
+  it { is_expected.to have_db_column :category }
 
-  it { is_expected.to have_many :products}
 end
