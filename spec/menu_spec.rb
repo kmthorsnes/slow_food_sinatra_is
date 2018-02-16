b1e510860524b40ca0a8a5ea302288c4bc145463
Antonella Morittu <morittuantonella@gmail.com>
2018-02-16 13:21:22 +0100

    added new branch add_to_plate

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -3,5 +3,7 @@ require 'spec_helper'
 describe Menu do
   it { is_expected.to have_db_column :name }
 
-  it { is_expected.to have_many :products}
+  it { is_expected.to have_db_column :price}
+
+  it { is_expected.to have_many :products}  
 end
